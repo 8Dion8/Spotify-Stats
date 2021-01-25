c7892e3401a93f1828763652ca6557c33f6a180b
8Dion8 <shvartserinfo@gmail.com>
2021-01-25 07:25:28 +0300

    finally getting something done

--- app.py
+++ app.py
@@ -2,11 +2,20 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import os
 from dotenv import load_dotenv
+import json
 
 load_dotenv()
 SECRET = os.getenv('SECRET')
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="fc75324c1ab44a2f915b28ca9da9de80",
-                                               client_secret="YOUR_APP_CLIENT_SECRET",
-                                               redirect_uri="YOUR_APP_REDIRECT_URI",
-                                               scope="user-library-read"))
+                                               client_secret=SECRET,
+                                               redirect_uri="https://www.google.it/",
+                                               scope="user-read-recently-played"))
+
+data = sp.current_user_recently_played(limit=50)
+
+with open('data.json', 'w') as f:
+    json.dump(data, f)
+
+
+print(data)
